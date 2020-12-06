6937549aaa1ea187affb41caa033fdf68bfae77a
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-12-06 18:39:34 +0100

    Practises

---
+++ basics/app.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var app_module_1 = require("./app_module");
+app_module_1.displaySomeData("I am from app.");
---
+++ basics/app.ts
@@ -0,0 +1,2 @@
+import {displaySomeData} from "./app_module"
+displaySomeData("I am from app.")
